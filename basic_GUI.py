32cdd3a519bb1b558873bb87b7cad8d41939d8b3
dimon-ton <saichon2namoom@gmail.com>
2022-05-26 21:19:40 +0700

    Python GUI EP.1

--- basic_GUI.py
+++ basic_GUI.py
@@ -1,1 +1,22 @@
+'''
+    นี่คือโปรแกรมแรกของเรา
+'''
+print('เริ่มโปรแกรม')
+
 from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+
+#-----------------------------------------------------------------
+GUI.title('โปรแกรมคำนวณเลข') #ชื่อโปรแกรม
+GUI.geometry('500x300') #ปรับขนาดโปรแกรม
+
+def Show():
+    messagebox.showinfo('Show Box','สวัสดีจ้า ได้แล้ว!!')
+
+B1 = ttk.Button(GUI,text="คลิกปุ่มนี้",command=Show)
+B1.pack(ipadx=30,ipady=30,pady=50)
+#-----------------------------------------------------------------
+
+GUI.mainloop()
